b7f6fa7f18e9940271a17ba60b5e4d15cd08ca28
jimurphy <jim.w.murphy@gmail.com>
2024-01-10 09:26:43 +0900

    add Gain.h

---
+++ Gain.h
@@ -0,0 +1,30 @@
+//
+// Gain.h
+// Gain Setter
+//
+// 
+
+#ifndef Gain_h
+#define Gain_h
+
+class Gain {
+public:
+    Gain(){
+    }
+    
+    ~Gain(){};
+    
+    inline void SetGain(float gain){
+        gainVal = gain;
+    }
+
+    inline void dsp(float input, float& output){
+        output = input * gainVal;
+    }
+    
+private:
+    float       gainVal;
+};
+
+
+#endif /* Gain_h */
