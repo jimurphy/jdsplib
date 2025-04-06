5d3a6714bea020a351b23070aed28d40bc873e75
jimurphy <jim.w.murphy@gmail.com>
2025-04-06 15:50:44 +1200

    Add tested and working Noise.h

--- Noise.h
+++ Noise.h
@@ -14,45 +14,49 @@
 
 class Noise{
     public:
-        Noise(){
-        }
+    Noise(){
+    }
         
-        ~Noise(){};
+    ~Noise(){};
 
-        inline void setSR(int _sr){
-            sr = _sr;
-        }
+    inline void setSR(int _sr){
+        sr = _sr;
+    }
+
+    inline void noiseInit(){
+        std::random_device rd;
+        gen = std::mt19937(rd());
+        dist = std::uniform_real_distribution<float>(-1.0f, 1.0f);
+    }
 
 	inline void setSeed(uint32_t _seed){
 	    fastseed = (_seed == 0) ? 7 : _seed; //prevent a seed of 0
-	    seedGen(); //generate a new seed for the MT
 	}
 
-        inline void setAmp(float _amp){
-            amp = _amp;
-        }
-    
-        inline void dsp(float& output){
-            output = amp * (dist(gen)); //Uses Mersenne Twister
-        }
+    inline void setAmp(float _amp){
+        amp = _amp;
+    }
+
+    inline void dsp(float& output){
+        output = dist(gen); //Uses Mersenne Twister
+    }
         
 	//Thanks to https://github.com/WebDrake/xorshift/tree/master
 	inline void fastRandom(float& output){
-	    seed ^= seed << 13;
-            seed ^= seed >> 17;
-            seed ^= seed << 15;
+	    fastseed ^= fastseed << 13;
+        fastseed ^= fastseed >> 17;
+        fastseed ^= fastseed << 15;
 
-	    output = amp*((seed >> 8) * (1.0f / 16777216.0f) * 2.0f - 1.0f); //scale to [-1,1]
+	    output = amp*((fastseed >> 8) * (1.0f / 16777216.0f) * 2.0f - 1.0f); //scale to [-1,1]
 	}
 	
     private:
         int       sr = 0;
-	uint32_t  fastseed = 2463534242UL; //seed for fastRandom xorshift
+	    uint32_t  fastseed = 2463534242UL; //seed for fastRandom xorshift
         float     amp = 1.0f;
 
-	std::random_device seedGen; //for mersenne twister
-        std::mt19937 gen(seedGen()); //mersenne twister prng
-	std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+        std::mt19937 gen; //mersenne twister prng
+	    std::uniform_real_distribution<float> dist;
     };
 
 #endif /* Noise_h */
